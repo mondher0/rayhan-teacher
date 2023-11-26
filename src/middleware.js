42d63ef28301a36893faf87704b9c1cd3b0ca47f
mondher04000 <m_mameri@estin.dz>
2023-11-26 14:54:18 +0100

    update

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,30 @@
 import createMiddleware from "next-intl/middleware";
 
-export default createMiddleware({
+import { NextResponse } from "next/server";
+
+const intlMiddleware = createMiddleware({
   // A list of all locales that are supported
   locales: ["en", "fr", "ar"],
 
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
   defaultLocale: "en",
 });
+
+export async function middleware(request) {
+  const token = request.cookies.get("token");
+  console.log(
+    "----------------------this is nextUrl----------------------",
+    request.nextUrl
+  );
+  console.log(
+    "----------------------this is url----------------------",
+    request.url
+  );
+  if (!token && !request.url.includes("/login")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return intlMiddleware(request);
+}
 
 export const config = {
   // Skip all paths that should not be internationalized. This example skips the
