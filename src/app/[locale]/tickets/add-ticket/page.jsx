326642f54c4783ea8027de3b36cc751d10ce7979
mondher <110062316+mondher0@users.noreply.github.com>
2023-12-06 22:40:36 +0100

    integrate create ticket

--- src/app/[locale]/tickets/add-ticket/page.jsx
+++ src/app/[locale]/tickets/add-ticket/page.jsx
@@ -1,41 +1,105 @@
 "use client";
 import { useLocale, useTranslations } from "next-intl";
 import "./add-ticket.css";
+import axiosInstance, { baseUrl } from "@/utils/constants";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "@/app/components/loader/Loader";
 
 const AddTicketPage = () => {
   const t = useTranslations("Index");
   const locale = useLocale();
+  const [services, setServices] = useState([]);
+  const [subject, setSubject] = useState("");
+  const [content, setContent] = useState("");
+  const [service, setService] = useState("");
+  const [importance, setImportance] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  // get services
+  const getService = async () => {
+    try {
+      const response = await axiosInstance.get(`${baseUrl}/ticket/services`);
+      console.log(response);
+      setServices(response?.data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // add ticket
+  const addTicket = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await axiosInstance.post(`${baseUrl}/ticket/store`, {
+        subject,
+        content,
+        service,
+        importance,
+      });
+      console.log(response);
+      setIsLoading(false);
+      toast.success("Ticket added successfully");
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      toast.error("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    getService();
+  }, []);
   return (
     <main>
+      <ToastContainer />
       <div className="add-ticket">
-        <form>
+        <form onSubmit={addTicket}>
           <div className="form-control">
             <div className="input-control">
               <label htmlFor="object">{t("ticketSubject")}</label>
-              <input type="text" name="object" id="object" />
+              <input
+                type="text"
+                name="object"
+                id="object"
+                onChange={(e) => setSubject(e.target.value)}
+              />
             </div>
             <div className="input-control">
               <label htmlFor="service">{t("service")}</label>
-              <select name="service" id="service">
-                <option value="1">Service 1</option>
-                <option value="2">Service 2</option>
-                <option value="3">Service 3</option>
-                <option value="4">Service 4</option>
+              <select
+                name="service"
+                id="service"
+                onChange={(e) => setService(e.target.value)}
+              >
+                <option>Choose servise</option>
+                {services.map((service) => (
+                  <option key={service} value={service}>
+                    {service}
+                  </option>
+                ))}
               </select>
             </div>
             <div className="input-control">
               <label htmlFor="importance">{t("importance")}</label>
-              <select name="importance" id="importance">
-                <option value="1">Service 1</option>
-                <option value="2">Service 2</option>
-                <option value="3">Service 3</option>
-                <option value="4">Service 4</option>
-              </select>
+              <input
+                type="text"
+                name="importance"
+                id="importance"
+                onChange={(e) => setImportance(e.target.value)}
+              />
             </div>
           </div>
           <div className="input-control">
             <label htmlFor="message">{t("message")}</label>
-            <textarea id="message" cols={40} rows={10}></textarea>
+            <textarea
+              id="message"
+              cols={40}
+              rows={10}
+              onChange={(e) => setContent(e.target.value)}
+            ></textarea>
           </div>
           <div
             className={
@@ -44,7 +108,9 @@ const AddTicketPage = () => {
                 : "btn-container"
             }
           >
-            <button className="btn">{t("send")}</button>
+            <button className="btn">
+              {isLoading ? <Loader /> : t("send")}
+            </button>
           </div>
         </form>
       </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,12 @@
+import axios from "axios";
+import { getCookie } from "cookies-next";
+
+// base url
 export const baseUrl = "https://back-dev.rayhaneelearning.com/api";
 
 // get local date and time
 export const getLocalDate = (date) => {
   const dateObj = new Date(date);
-
   // Format the Date object to the desired format
   const formattedDate = `${("0" + dateObj.getDate()).slice(-2)}/${(
     "0" +
@@ -14,3 +17,20 @@ export const getLocalDate = (date) => {
   ).slice(-2)}`;
   return { formattedDate, formattedTime };
 };
+
+// axios instance
+const axiosInstance = axios.create();
+const TOKEN_KEY = "token";
+axiosInstance.interceptors.request.use((request) => {
+  const token = getCookie(TOKEN_KEY);
+  console.log(token);
+  if (request.headers) {
+    request.headers["Authorization"] = `Bearer ${token}`;
+  } else {
+    request.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+  }
+  return request;
+});
+export default axiosInstance;
